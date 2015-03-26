2bb9b25a6e981df31a0008720d9dc5a6853057ed
Gurpreet Singh <gurpreetsingh_12644@aitpune.edu.in>
2015-03-26 18:51:37 +0530

    added html interactivity with javaScript

--- main.py
+++ main.py
@@ -145,21 +145,24 @@ def fillSub():
 
 def calcSub(sub):
 	if sub.sbType == 'PP':
-		stuPass = 0
-		stuFail = 0
-		stuAbsent = 0
-		avgMidSemMrks = -1
-		avgEndSemMrks = -1
-		avgTotalMrks = -1
-		totalMidSemMrks = 0
-		totalEndSemMrks = 0
-		midSemList = [0] * 31
-		endSemList = [0] * 71
-		totalMrksList = [0] * 101
-		maxMidSemMrks = 0
-		minMidSemMrk0s = 30
-		maxEndSemMrks = 0
-		minEndSemMrks = 70
+		stuPass 	= 0
+		stuFail 	= 0
+		stuAbsent 	= 0
+		midSemList 	= [0] * 31
+		endSemList 	= [0] * 71
+		totalMrksList 	= [0] * 101
+		maxMidSemMrks 	= 0
+		minMidSemMrks 	= 30
+		avgMidSemMrks 	= -1
+		maxEndSemMrks 	= 0
+		minEndSemMrks 	= 70
+		avgEndSemMrks 	= -1
+		maxTotalMrks  	= 0
+		minTotalMrks	= 100
+		avgTotalMrks	= -1
+		midSemMrksSum	= 0
+		endSemMrksSum	= 0
+		
 		for st in sub.stus:
 			print(st[2][0], st[2][1])
 			if st[2][0].isdigit() and st[2][1].isdigit() and st[2][0] != "AA" and st[2][1] != "AA":
@@ -169,39 +172,66 @@ def calcSub(sub):
 					stuPass += 1
 				else:
 					stuFail += 1
-				totalMidSemMrks += midSemMrks
-				totalEndSemMrks += endSemMrks
 				midSemList[midSemMrks] += 1
 				endSemList[endSemMrks] += 1
 				t = midSemMrks + endSemMrks
 				totalMrksList[t] += 1
+				if maxMidSemMrks < midSemMrks: maxMidSemMrks = midSemMrks
+				if minMidSemMrks > midSemMrks: minMidSemMrks = midSemMrks
+				if maxEndSemMrks < endSemMrks: maxEndSemMrks = endSemMrks
+				if maxEndSemMrks > endSemMrks: maxMidSemMrks = midSemMrks
+				if maxTotalMrks < t: maxEndSemMrks = t
+				if maxTotalMrks > t: maxMidSemMrks = t
+				midSemMrksSum += midSemMrks
+				endSemMrksSum += endSemMrks
 			else:
 				stuAbsent += 1
-		stuPsnt = stuPass + stuFail
-		avgMidSemMrks = totalMidSemMrks / stuPsnt
-		avgEndSemMrks = totalEndSemMrks / stuPsnt
-		result = open("summary.txt", 'w')
-		result.write("\tTotal Student : %s\n" % str(stuPsnt + stuAbsent))
-		result.write("\tStudents Pass : %s\n" % str(stuPass))
-		result.write("\tStudent Fail  : %s\n" % str(stuFail))
-		result.write("\tStudent Absent: %s\n" % str(stuAbsent))
-		result.write("\tMin Marks     : %s\n" % str(min(totalMrksList)))
-		result.write("\tMax Marks     : %s\n" % str(max(totalMrksList)))
-		result.write("\tAverage MidSemMrks : %5.2f\n" % avgMidSemMrks)
-		result.write("\tAverage EndSemMrks : %5.2f\n" % avgEndSemMrks)
-		result.write("\tAverage Marks      : %5.2f\n" % avg(totalMrksList))
-		result.write("\n\tMidSemList\n")
-		for i in range(31):
-			result.write("\t%s\t%s\n" %(str(i).rjust(4), str(midSemList[i]).rjust(4)))
-		result.write("\n\tEndSemList\n")
-		for i in range(71):
-			result.write("\t%s\t%s\n" %(str(i).rjust(4), str(endSemList[i]).rjust(4)))
-		result.write("\n\ttotalMrksList\n")
-		for i in range(101):
-			result.write("\t%s\t%s\n" %(str(i).rjust(4), str(totalMrksList[i]).rjust(4)))
-		result.write("- . "*30)
-		result.close()
 
+		stuPsnt = stuPass + stuFail
+		
+		webPage = open("page.html", 'w')
+		webPage.write("""
+<html>
+<head>
+	<title>Digital Communictaion</title>
+	<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+	<h1 align="center"> Digital Communication </h1>
+	<table>	
+	<tr>
+		<td><table>
+			<tr><td>Total Student</td>	<td>%s</td></tr>
+			<tr><td>Students Pass</td>	<td>%s</td></tr>
+			<tr><td>Student Fail</td>	<td>%s</td></tr> 
+			<tr><td>Student Absent</td>	<td>%s</td></tr>
+			<tr><td>Min Marks</td>		<td>%s</td></tr>
+			<tr><td>Max Marks</td>		<td>%s</td><tr>
+		</table></td>
+		<td><table>
+			<tr><td><button onclick="changeImgLeft()"> < </button></td>
+			<td><img id="theImage" src="midSem.png"></img></td>
+			<td><button onclick="changeImgRight()"> > </button></td></tr>
+		</table></td>
+	</tr>
+	</table>
+</body>
+</html>		
+		""" % (str(stuPsnt + stuAbsent), str(stuPass), str(stuFail), str(stuAbsent), minMidSemMrks, maxMidSemMrks))
+		webPage.close()
+		
+		scriptFile = open("script.js", 'w')
+		scriptFile.write("""
+	img_array= new Array("endSem.png", "totalMrks.png", "midSem.png");
+	i=0;
+	function changeImgLeft(){
+		document.getElementById("theImage").src=img_array[i];
+		i = (i + 2) % 3;
+	}
+	function changeImgRight(){
+		document.getElementById("theImage").src=img_array[i];
+		i = (i + 1) % 3;
+	}""")
 		
 		plt.bar(range(len(midSemList)), midSemList, align='center', width=.5)
 		plt.title('Mid Sem Marks Distrubution')
@@ -266,5 +296,5 @@ if __name__ == "__main__":
 	getStu()
 	fillBranches()
 	fillSub()
-	#calcSub(subList[0])
-	createDirectoryStructure()
+	calcSub(subList[0])
+	#createDirectoryStructure()
